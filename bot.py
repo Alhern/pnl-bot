a4ca8ebb8a372e53dfe0bbfaa7fc10a614e80619
Alhern <23422910+Alhern@users.noreply.github.com>
2018-10-01 12:41:29 +0200

    added keys so the script can connect to twitter

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random, tweepy, creds.py
+import random, tweepy
+from creds import *
 
 lion = open("lion.txt", "r")
 onizuka = open("onizuka.txt", "r")
@@ -12,6 +13,16 @@ oniLines = onizuka.readlines()
 allLines = [lionLines, oniLines]
 
 print(random.choice(random.choice(allLines)))
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+user = api.me()
+print(user.name)
 
 lion.close()
 onizuka.close()
