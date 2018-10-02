8b2bc0641991f909f5a74971e765befc63926313
Alhern <23422910+Alhern@users.noreply.github.com>
2018-10-02 10:40:09 +0200

    bot can now tweet every time it's called, general code improvements, added comments

--- bot.py
+++ bot.py
@@ -1,28 +1,36 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random, tweepy
+import random
+import tweepy
 from creds import *
 
+# The songs' files that will be used:
 lion = open("lion.txt", "r")
 onizuka = open("onizuka.txt", "r")
 
+
+# Catching all the lines for each song:
 lionLines = lion.readlines()
 oniLines = onizuka.readlines()
 
+
+# Regrouping all the lines in one list:
 allLines = [lionLines, oniLines]
 
-print(random.choice(random.choice(allLines)))
 
+# Authenticating
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
 
-user = api.me()
-print(user.name)
+# Time to tweet:
+def tweet():
+    api.update_status(random.choice(random.choice(allLines)))
+
+
+tweet()
 
 lion.close()
 onizuka.close()
