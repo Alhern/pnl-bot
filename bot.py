1331efb087a407cdf9d3825cca668d3e1d2285bb
Alhern <23422910+Alhern@users.noreply.github.com>
2019-10-03 15:49:49 +0200

    Modularized - lyrics data has been exported to data.py

--- bot.py
+++ bot.py
@@ -5,25 +5,7 @@ import random
 import tweepy
 from time import sleep
 from creds import *
-
-# The songs' files that will be used:
-lion = open("lion.txt", "r")
-onizuka = open("onizuka.txt", "r")
-ninety1s = open("91s.txt", "r")
-deconnecte = open("deconnecte.txt", "r")
-kutaubud = open("kutaubud.txt", "r")
-
-
-# Catching all the lines for each song:
-lionLines = lion.readlines()
-oniLines = onizuka.readlines()
-ninety1sLines = ninety1s.readlines()
-deconnecteLines = deconnecte.readlines()
-kutaubudLines = kutaubud.readlines()
-
-
-# Regrouping all the lines in one list:
-allLines = [lionLines, oniLines, ninety1sLines, deconnecteLines, kutaubudLines]
+from data import *
 
 
 # Authenticating
@@ -47,10 +29,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-lion.close()
-onizuka.close()
-ninety1s.close()
-deconnecte.close()
-kutaubud.close()
